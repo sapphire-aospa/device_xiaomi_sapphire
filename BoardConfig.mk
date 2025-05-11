16b7bb1e553e6ed4788b24ad453d8bbbb1b282df
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-05-11 12:02:08 +0545

    Update BoardConfig.mk
    Signed-off-by: Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,7 @@ $(call soong_config_set, ufsbsg, ufsframework, bsg)
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 
 # Camera
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+$(soong_config_set camera override_format_from_reserved, true)
 
 # Display
 SOONG_CONFIG_NAMESPACES += qcomDisplayVars
