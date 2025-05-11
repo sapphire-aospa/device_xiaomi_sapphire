a08e39c2963cc62f8af01f781ba2a291a272c596
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-05-11 03:12:57 -0400

    sapphire: Address missing SetTaskProfiles symbol in camera libs
    The camera HAL forced stop because it could not load
    /vendor/lib64/hw/camera.xiaomi.so which is due to the missing
     symbol referenced in several libraries:
    
    fiqri@Fiqri:~/proprietary_vendor_xiaomi_marble$ grep -i -r SetTaskProfiles
    grep: proprietary/vendor/lib64/hw/com.qti.chi.override.so: binary file matches
    grep: proprietary/vendor/lib64/libmialgoengine.so: binary file matches
    grep: proprietary/vendor/lib64/libcamxcommonutils.so: binary file matches
    
    This is because DECLS was removed in [1] on Android 15 QPR2, and
    caused SetTaskProfiles() to be hidden, so shim SetTaskProfiles
    with libprocessgroup_shim.
    
    [1]: https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    Change-Id: Ic46aae47bdf6be09736fe89d798cb7017f07cc6c
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
+        vendor/lib64/hw/com.qti.chi.override.so|vendor/lib64/libcamxcommonutils.so|vendor/lib64/libmialgoengine.so|vendor/lib64/hw/camera.qcom.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libprocessgroup_shim.so" "${2}" || "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "${2}"
+            ;;
         vendor/lib64/vendor.libdpmframework.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
