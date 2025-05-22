3bc78deae2f65bdd88fe8c0beba692f950ac7a15
Angelpro09xd <aloplop090708@gmail.com>
2025-05-22 23:41:37 +0200

    sapphire: sepolicy: fix sepolicy detection for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,7 +207,7 @@ BOOT_SECURITY_PATCH := 2025-05-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
--include device/qcom/sepolicy_vndr/sm6225/SEPolicy.mk
+include device/qcom/sepolicy_vndr/sm6225/SEPolicy.mk
 include device/xiaomi/sepolicy/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
